eda32711f0507cc0ef6d5ff9fe43b7aa6e66a351
priyanka1565 <priyankaingle250@gmail.com>
2023-04-12 14:21:04 +0530

    get userbyid  bugs fixed

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -2,18 +2,35 @@ const User = require("../model/UserModel");
 
 //create a user
 
-const CreateUser = async (req, res) => {
-    const { name, email, bio } = req.body;
-    try {
-        const newUser = new User({ name, email, bio });
-        const saveUser = await newUser.save();
-        res.status(201).json(saveUser)
-        
-    }
-    catch (error) {
-        res.status(401).json({ message: error.message });
-        
-    }
+const createUser = async (req, res) => {
+  const { name, email, bio } = req.body;
+  try {
+    const newUser = new User({ name, email, bio });
+    const saveUser = await newUser.save();
+    res.status(201).json(saveUser);
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
 
-}
-module.exports = CreateUser;
+//passing the id in params and get the user
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  getUserById,
+};
+  
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -2,7 +2,13 @@
 const express = require("express");
 const router = express.Router();
 // Import controller for user
-const createUser  = require("../controller/UserController"); 
+const {
+  createUser,
+  getUserById,
+ 
+} = require("../controller/UserController")
 router.post("/createUser", createUser);
+router.get("/:id", getUserById);
+
 
 module.exports = router;
